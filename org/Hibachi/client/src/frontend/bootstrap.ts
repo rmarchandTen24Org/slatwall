e1cdb7f366665454f08248e5f00d1b2ef995050a
ian-a-hickey <ian.hickey@ten24web.com>
2015-12-08 16:34:25 -0500

    saving new bootstrap file

--- org/Hibachi/client/src/frontend/bootstrap.ts
+++ org/Hibachi/client/src/frontend/bootstrap.ts
@@ -6,7 +6,6 @@ import{frontendmodule} from "./frontend/frontend.module";
 
 declare var slatwallAngular:any;
 
-
 //custom bootstrapper
 class bootstrapper{
     constructor(){
@@ -16,22 +15,24 @@ class bootstrapper{
       });
     }
     //should contain any data that is required by angular prior to bootstrapping
-   fetchData =()=> {
-       var initInjector = angular.injector(["ng"]);
-       var $http = initInjector.get<ng.IHttpService>("$http");
-       var $q = initInjector.get<ng.IQService>("$q");
+    fetchData =()=> {
+        var initInjector = angular.injector(["ng"]);
+        var $http = initInjector.get<ng.IHttpService>("$http");
+        var $q = initInjector.get<ng.IQService>("$q");
    
-      var deferred = $q.defer();
-      var urlString = '/index.cfm/?slatAction=api:main.getModel';
-      var params = {};
-      $http.get(urlString,{
-          params:params
-      }).success((data)=>{
-          deferred.resolve(data);
-      }).error((reason)=>{
-          deferred.reject(reason);
-      });
-      return deferred.promise;
+        var deferred = $q.defer();
+        var urlString = '/index.cfm/?slatAction=api:main.getModel';
+        var params = {};
+      
+        $http.get(urlString,{
+            params:params
+        }).success((data)=>{
+            deferred.resolve(data);
+        }).error((reason)=>{
+            deferred.reject(reason);
+        });
+        
+        return deferred.promise;
    }
     
     bootstrapApplication = ()=> {
