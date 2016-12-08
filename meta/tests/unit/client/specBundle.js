b6bebad793557cb0ff43ae877fae1194dcc646bb
Ryan Marchand <ryan.marchand@ten24web.com>
2016-12-08 14:58:06 -0500

    saving owrk

---
+++ meta/tests/unit/client/specBundle.js
@@ -0,0 +1,10 @@
+describe('my first test', function () {
+    it('my first exprectations', function () {
+        expect(true).toBeTruthy('What? Why was true not truthy?');
+    });
+});
+describe('my second test', function () {
+    it('my second exprectation', function () {
+        expect(true).toBeTruthy('What? Why was true not truthy?');
+    });
+});
