e85d7af135a58030f0f5a1d682aaf23b03279de3
Ryan Marchand <ryan.marchand@ten24web.com>
2017-01-17 22:11:27 -0500

    updating config

--- admin/client/webpack2.config.js
+++ admin/client/webpack2.config.js
@@ -1,10 +1,25 @@
 
 var path = require('path');
+var PATHS = {
+    app: path.join(__dirname, '/src'),
+    lib: path.join(__dirname, '/lib')
+};
+
+var appConfigService = {
+	addVendor: function (appConfig,name, vendorPath) {
+        appConfig.resolve.alias[name] =  path.join(PATHS.lib, vendorPath);
+        appConfig.entry.app.splice(appConfig.entry.app.length - 1, 0, name);
+        return this;
+    },
+}
+
 var devConfig = require('../../org/Hibachi/client/webpack2.config');
-console.log(devConfig);
-//devConfig.addVendor('jquery-ui-timepicker-addon','../../HibachiAssets/js/jquery-ui-timepicker-addon-1.3.1.js')
-   // .addVendor('jquery-typewatch','../../HibachiAssets/js/jquery-typewatch-2.0.js')
-//;
+devConfig.output.path=PATHS.app;
+
+
+appConfigService.addVendor(devConfig,'jquery-ui-timepicker-addon','../../HibachiAssets/js/jquery-ui-timepicker-addon-1.3.1.js')
+.addVendor(devConfig,'jquery-typewatch','../../HibachiAssets/js/jquery-typewatch-2.0.js')
+;
 devConfig.entry.app=__dirname + "/src/bootstrap.ts";
 module.exports = devConfig;
 //.setupApp(__dirname);
